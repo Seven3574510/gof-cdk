dbdab80ebbbcf16468e12df18c4e9f45cbf7a0bb
LeekJay <314964866@qq.com>
2025-12-28 01:50:41 +0800

    fix(api): 添加 Origin 和 Referer 请求头以解决 403 错误
    - 在 API 请求中添加 Origin: https://wjdr-giftcode.centurygames.cn
    - 在 API 请求中添加 Referer: https://wjdr-giftcode.centurygames.cn/
    - 更新测试文件同步添加相同的请求头配置
    
    修复 #4

--- src/api.ts
+++ src/api.ts
@@ -126,6 +126,8 @@ class ApiService {
 				"User-Agent":
 					"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36",
 				"Content-Type": "application/x-www-form-urlencoded",
+				Origin: "https://wjdr-giftcode.centurygames.cn",
+      			Referer: "https://wjdr-giftcode.centurygames.cn/"
 			},
 			timeout: this.timeout,
 		});
--- tests/test.ts
+++ tests/test.ts
@@ -1,48 +1,46 @@
-import axios from "axios";
-import { DdddOcr } from "ddddocr-node";
-import { generateSignedObject } from "../src/utils";
-import { useLogger } from "../src/logger";
+import axios from 'axios';
+import { DdddOcr } from 'ddddocr-node';
+import { generateSignedObject } from '../src/utils';
+import { useLogger } from '../src/logger';
 
-const logger = useLogger("test");
+const logger = useLogger('test');
 
 const ocr = new DdddOcr();
 
 async function test() {
-	const instance = axios.create({
-		baseURL: "https://wjdr-giftcode-api.campfiregames.cn/api",
-		headers: {
-			Accept: "application/json, text/plain, */*",
-			"User-Agent":
-				"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36",
-			"Content-Type": "application/x-www-form-urlencoded",
-		},
-		timeout: 20000,
-	});
+  const instance = axios.create({
+    baseURL: 'https://wjdr-giftcode-api.campfiregames.cn/api',
+    headers: {
+      Accept: 'application/json, text/plain, */*',
+      'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36',
+      'Content-Type': 'application/x-www-form-urlencoded',
+      Origin: 'https://wjdr-giftcode.centurygames.cn',
+      Referer: 'https://wjdr-giftcode.centurygames.cn/'
+    },
+    timeout: 20000
+  });
 
-	const data = generateSignedObject(
-		{ fid: "43451091", time: Date.now() },
-		"Uiv#87#SPan.ECsp",
-	);
+  const data = generateSignedObject({ fid: '43451091', time: Date.now() }, 'Uiv#87#SPan.ECsp');
 
-	const playerInfo = await instance.post("/player", data);
-	const captcha = await instance.post("/captcha", data);
-	const captchaResult = await ocr.classification(captcha.data.data.img);
-	const giftData = generateSignedObject(
-		{
-			fid: "43451091",
-			cdk: "HAPPY61",
-			captcha_code: captchaResult,
-			time: Date.now(),
-		},
-		"Uiv#87#SPan.ECsp",
-	);
-	const giftResult = await instance.post("/gift_code", giftData);
+  const playerInfo = await instance.post('/player', data);
+  const captcha = await instance.post('/captcha', data);
+  const captchaResult = await ocr.classification(captcha.data.data.img);
+  const giftData = generateSignedObject(
+    {
+      fid: '43451091',
+      cdk: 'HAPPY61',
+      captcha_code: captchaResult,
+      time: Date.now()
+    },
+    'Uiv#87#SPan.ECsp'
+  );
+  const giftResult = await instance.post('/gift_code', giftData);
 
-	logger.info(playerInfo.data.data);
-	logger.info(captchaResult);
-	logger.info(giftResult.data);
+  logger.info(playerInfo.data.data);
+  logger.info(captchaResult);
+  logger.info(giftResult.data);
 }
 
 test().finally(() => {
-	logger.result("测试完成");
+  logger.result('测试完成');
 });
